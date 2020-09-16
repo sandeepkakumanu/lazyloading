90c9c0faf4aa10e717618eb461bfb58267873961
admin <admin@corp.ojas-it.com>
2020-09-16 16:32:46 +0530

    added files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path:'home',
+    loadChildren: () =>
+    import ('./home/home.module') .then(m => m.HomeModule)
+  },
+  {
+    path:'about',
+    loadChildren: () =>
+    import ('./about/about.module') .then(m => m.AboutModule)
+  },
+  {
+    path:'',
+    redirectTo:"",
+    pathMatch:"full"
+
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
